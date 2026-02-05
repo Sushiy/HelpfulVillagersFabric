8cc328038268dcbe970c5ea9939678eae89dfeba
Julian <julian.viezens@btf.de>
2026-02-05 01:17:10 +0100

    Replaced with generated mod

---
+++ src/client/java/sushiy/helpfulvillagersfabric/HelpfulVillagersFabricClient.java
@@ -0,0 +1,10 @@
+package sushiy.helpfulvillagersfabric;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HelpfulVillagersFabricClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/sushiy/helpfulvillagersfabric/HelpfulVillagersFabricDataGenerator.java
@@ -0,0 +1,11 @@
+package sushiy.helpfulvillagersfabric;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HelpfulVillagersFabricDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
